18f63347195385a0d38531c928d3f44dd069220d
Markus Fruhmann <frufrufruchtjoghurt.git@frufru.dev>
2025-12-08 12:23:14 +0100

    [Day 7] Added Dequeue for HashSet changes

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,8 +1,10 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 pub fn solve_part1(input: &str) -> usize {
     let mut beams: HashSet<usize> = HashSet::with_capacity(1);
     let mut split_count = 0;
+    let mut add_beams: VecDeque<usize> = VecDeque::with_capacity(1024);
+    let mut remove_beams: VecDeque<usize> = VecDeque::with_capacity(1024);
 
     let start_pos = input.find('S').unwrap();
     beams.insert(start_pos);
@@ -13,21 +15,28 @@ pub fn solve_part1(input: &str) -> usize {
         .filter(|(y, _)| y % 2 == 0)
         .skip(1)
         .for_each(|(_, line)| {
-            beams = beams
-                .drain()
-                .filter_map(|beam| {
-                    let char_at_beam = line.chars().nth(beam).unwrap();
-                    match char_at_beam {
-                        '.' => Some(vec![beam]),
-                        '^' => {
-                            split_count += 1;
-                            Some(vec![beam - 1, beam + 1])
-                        }
-                        _ => None,
+            for &beam in &beams {
+                let char_at_beam = line.chars().nth(beam).unwrap();
+                match char_at_beam {
+                    '.' => {}
+                    '^' => {
+                        add_beams.push_back(beam - 1);
+                        add_beams.push_back(beam + 1);
+                        remove_beams.push_back(beam);
+                        split_count += 1;
                     }
-                })
-                .flatten()
-                .collect();
+                    _ => {
+                        remove_beams.push_back(beam);
+                    }
+                }
+            }
+
+            for beam in remove_beams.drain(..) {
+                beams.remove(&beam);
+            }
+            for beam in add_beams.drain(..) {
+                beams.insert(beam);
+            }
 
             // _pretty_print(line, &beams);
         });
